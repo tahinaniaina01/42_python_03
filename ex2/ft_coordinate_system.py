4980fd71588a838f935c69dd3e322187ff638482
trakotos <trakotos@student.42antananarivo.mg>
2026-03-30 17:58:25 +0300

    add ft_coordinate_system.py: implement coordinate input and distance calculation functionality

---
+++ ex2/ft_coordinate_system.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# ########################################################################### #
+#   shebang: 1                                                                #
+#                                                          :::      ::::::::  #
+#   ft_coordinate_system.py                              :+:      :+:    :+:  #
+#                                                      +:+ +:+         +:+    #
+#   By: trakotos <trakotos@student.42antananarivo.   +#+  +:+       +#+       #
+#                                                  +#+#+#+#+#+   +#+          #
+#   Created: 2026/03/29 16:19:27 by trakotos            #+#    #+#            #
+#   Updated: 2026/03/30 17:57:41 by trakotos           ###   ########.fr      #
+#                                                                             #
+# ########################################################################### #
+
+
+import math
+
+
+class InputError(Exception):
+    pass
+
+
+def get_player_pos() -> tuple[float, float, float]:
+    coords: list[int] = []
+    datas: list[str] = []
+    res: str = ""
+    for c in input("Enter new coordinates as floats in format 'x,y,z': "):
+        if c != ",":
+            res += c
+        else:
+            datas += [res]
+            res = ""
+    datas += [res]
+    if len(datas) != 3:
+        raise InputError("Invalid syntax")
+    for data in datas:
+        val: float = 0.0
+        try:
+            val = float(data)
+        except ValueError as e:
+            raise ValueError(f"Error on parameter '{data}': {e}")
+        coords += [val]
+    return (coords[0], coords[1], coords[2])
+
+
+def test_get_plaer_pos() -> tuple[float, float, float]:
+    coord: tuple[float, float, float] | None = None
+    while coord is None:
+        try:
+            coord = get_player_pos()
+        except InputError as e:
+            print(e)
+        except ValueError as e:
+            print(e)
+    return coord
+
+
+def calcul_distance(
+    v1: tuple[float, float, float], v2: tuple[float, float, float]
+) -> float:
+    x1, y1, z1 = v1
+    x2, y2, z2 = v2
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
+
+
+if __name__ == "__main__":
+    print("=== Game Coordinate System ===\n")
+    print("Get a first set of coordinates")
+    center = (0.0, 0.0, 0.0)
+    coord1 = test_get_plaer_pos()
+    distance = round(calcul_distance(center, coord1), 4)
+    x1, y1, z1 = coord1
+    print(f"Got a first tuple: {coord1}")
+    print(f"It includes: X={x1}, Y={y1}, Z={z1}")
+    print(f"Distance to center: {distance}")
+    print()
+    print("Get a second set of coordinates")
+    coord2 = test_get_plaer_pos()
+    distance = round(calcul_distance(coord1, coord2), 4)
+    print(f"Distance between the 2 sets of coordinates: {distance}")
